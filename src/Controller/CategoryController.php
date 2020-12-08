f48decd315134c4c436df2105765691d4fb07a0f
Schyzofrania <steve.crepin@gmail.com>
2020-12-08 10:45:13 +0100

    ajout formulaire category et program

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CategoryType;
 use App\Entity\Category;
 use App\Entity\Program;
 
@@ -26,6 +28,24 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig', [
             'categories' => $category
         ]);
+    }
+    /**
+     * The controller for the category add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    { 
+        $category = new Category(); 
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', ["form" => $form->createView()]);
     }
     /**
      * Getting a program by id
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,8 +6,10 @@ use App\Entity\Season;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Category;
+use App\Form\ProgramType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 /**
@@ -33,6 +35,24 @@ class ProgramController extends AbstractController
             ['programs'=> $programs,]
         );
         
+    }
+        /**
+     * The controller for the category add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    { 
+        $program = new Program(); 
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($program);
+            $entityManager->flush();
+            return $this->redirectToRoute('program_index');
+        }
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
     }
     /**
      * Getting a program by id
